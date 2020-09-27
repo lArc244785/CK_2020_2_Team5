b6a29ad1092dba70fa6bec5069ef5716273f2387
isolesty <isolesty0814@gamil.com>
2020-09-27 18:35:50 +0900

    VFX_ cannon, dash, fire, hit

---
+++ Assets/Game/Scenes/Jun_test/VFX/Script/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject enemy;
+
+    public GameObject effect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject cannonEffect = Instantiate(effect, other.transform.position, enemy.transform.rotation);
+
+    }
+}
---
+++ Assets/Game/Scenes/Jun_test/VFX/Script/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject cannon;
+
+    public Transform firePos;
+
+    public GameObject effect;
+
+    Vector3 shoot;
+
+    private void Start()
+    {
+        //Instantiate(cannon, firePos.transform.position, firePos.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //생성
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(cannon, firePos.transform.position, firePos.transform.rotation);
+
+            GameObject cannonEffect = Instantiate(effect, firePos.transform.position, firePos.transform.rotation);
+
+            Destroy(cannonEffect,2f) ;
+        }
+    }
+}
