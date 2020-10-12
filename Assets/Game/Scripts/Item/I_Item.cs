6a0bf9a8eb223143b0afc09a7a4068e72dec9b29
lArc <jlky8932@gmail.com>
2020-10-13 08:48:16 +0900

    Final_1

--- Assets/Game/Scripts/Item/I_Item.cs
+++ Assets/Game/Scripts/Item/I_Item.cs
@@ -10,3 +10,26 @@ public class I_Item : MonoBehaviour
     public virtual void Event() { }
 
 }
+//Stage A
+//2.5   4
+//2.3   6.7
+
+//Stage B
+//4     6.2
+//13.7  7.8
+
+//Stage C
+//7     4
+//0.5   6.8
+
+//Stage D 
+//7.25      8.5
+//-0.5      1.25
+
+//Stage E
+//7.7   8.5
+//-2.64 1.25
+
+
+//0 0.7
+//13.5  1.3
